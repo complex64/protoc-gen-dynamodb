1b308de4af523254f81dcfffe4110a0d358782df
Joe <joe@complex64.com>
2024-01-08 22:37:43 +0100

    buf: update to v1.28.1

--- proto/gen.go
+++ proto/gen.go
@@ -1,6 +1,6 @@
 package proto
 
-//go:generate go install github.com/bufbuild/buf/cmd/buf@v1.27.1
+//go:generate go install github.com/bufbuild/buf/cmd/buf@v1.28.1
 //go:generate buf lint
 //go:generate find .. -name '*.pb.go' -delete
 //go:generate buf generate
